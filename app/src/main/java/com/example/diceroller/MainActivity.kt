d61b8aa35da57229f16a7e4fcfda2ec1464d0b0a
macky-boi <125427131+macky-boi@users.noreply.github.com>
2023-08-02 12:42:05 +0800

    Dice Image + Vector Drawables

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,17 +16,21 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener {
-            val randomInt = Random().nextInt(6) + 1
-            val drawableResource = when (randomInt) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-            val diceImage : ImageView = findViewById(R.id.dice_image)
-            diceImage.setImageResource(drawableResource)
+            rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+    fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
